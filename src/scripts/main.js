3e0df144ca5d11a29538a205cece51a9e9229f19
CườngTrần <interstar0406@gmail.com>
2018-12-06 21:45:43 +0700

    fix tab

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -93,7 +93,6 @@ function get_API_img() {
 			}
 			$("#responseTextArea_2").text(JSON.stringify(faceid_of_you, null, 4));
 		})
-
 		.fail(function (jqXHR, textStatus, errorThrown) {
 			// Display error message.
 			var errorString = (errorThrown === "") ?
@@ -120,7 +119,7 @@ function get_API_img() {
 			},
 			type: "POST",
 			// Request body.
-			data: '{"faceId1": "64576a58-55b8-4dc5-aa98-a5bf31018bea","faceId2": "d60ede49-e9b5-48e4-9d2b-6f426c2f3df3"}',
+			data: '{"faceId1": "d60ede49-e9b5-48e4-9d2b-6f426c2f3df3","faceId2": "d60ede49-e9b5-48e4-9d2b-6f426c2f3df3"}',
 			// {
 			// 	"faceId1": "c5c24a82-6845-4031-9d5d-978df9175426",
 			// 	"faceId2": "815df99c-598f-4926-930a-a734b3fd651c"
